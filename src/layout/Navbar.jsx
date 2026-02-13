7f7124c42c7ed39e406faaa927c8ae59e5895591
Adolfo <fragosoadolfo1@gmail.com>
2026-02-13 13:00:47 -0600

    created demo flag

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -12,9 +12,27 @@ const Navbar = () => {
 
     return (
         <nav className="navbar">
-            <Link to="/" className="navbar-brand">
-                Pooly
-            </Link>
+            <div style={{ display: 'flex', alignItems: 'center', gap: '0.75rem' }}>
+                <Link to="/" className="navbar-brand">
+                    Pooly
+                </Link>
+                <span style={{
+                    background: '#059669',
+                    color: 'white',
+                    fontSize: '0.8125rem',
+                    fontWeight: 700,
+                    padding: '0.25rem 0.875rem',
+                    borderRadius: '4px 0 0 4px',
+                    letterSpacing: '0.05em',
+                    lineHeight: '1.4',
+                    position: 'relative',
+                    userSelect: 'none',
+                    clipPath: 'polygon(0 0, calc(100% - 8px) 0, 100% 50%, calc(100% - 8px) 100%, 0 100%)',
+                    paddingRight: '1.25rem',
+                }}>
+                    DEMO
+                </span>
+            </div>
 
             <div className="navbar-links">
                 {user ? (
